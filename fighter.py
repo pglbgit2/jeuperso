a8ab2f31025feccd2980e482e5f5bd0e064f9f3d
pglbgit2 <pierre.gilles.le.botlan@gmail.com>
2024-02-29 19:30:32 +0100

    creating fighter with races str

--- fighter.py
+++ fighter.py
@@ -1,23 +1,33 @@
-import armors, items, weapons
+import armors, items, weapons, races
 from typing import List, Union
 
 
+FACTIONS = []
 
 class FIGHTER:
-    def __init__(self, _name, gold = 0, _HP:int =20, _MaxHP:int =20, _Stamina:int =5, _magic:int =0, _stamina_regeneration:int =5, _race :str = "Human",  _Equipment: List[items.ITEM] = [], _Inventory: List[items.ITEM] = []):
-        self.HP = _HP
-        self.MaxHP = _MaxHP
-        self.stamina = _Stamina
-        self.magic = _magic
-        self.stamina_regeneration = _stamina_regeneration
-        self.equipment = _Equipment
-        self.inventory = _Inventory
-        self.race = _race
-        self.name = _name
+    def __init__(self, name:str, faction:str, gold:int = 0, HP:int =20, MaxHP:int =20, Stamina:int =5, magic:int =0, stamina_regeneration:int =5, race :str = "Human",  Equipment: List[Union[armors.ARMOR, weapons.WEAPON]]= [], Inventory: List[items.ITEM] = []):
+        self.HP = HP
+        self.MaxHP = MaxHP
+        self.stamina = Stamina
+        self.magic = magic
+        self.stamina_regeneration = stamina_regeneration
+        self.equipment = Equipment
+        self.inventory = Inventory
+        self.race = race
+        self.name = name
         self.money = gold
+        self.faction = faction
         
     def equip(self, stuff : Union[armors.ARMOR, weapons.WEAPON]):
         self.equipment.append(stuff)
         
     def get_item(self, stuff : items.ITEM):
         self.equipment.append(stuff)
+        
+    @staticmethod
+    def instantiate_from_race(race:str, name:str, faction: str):
+        if race in races.RACES:
+            Race = getattr(races, race)
+            return FIGHTER(name = name, faction= faction, **Race)
+        
+#billy = FIGHTER.instantiate_from_race("HUMAN", "billy", "team1")
--- races.py
+++ races.py
@@ -2,12 +2,18 @@
 
 ### RACES & DEFAULT CHARACTER CLASS ###
 
+
 ## HUMANS ##
 HUMAN  = {
-'HP' : 20,
-"MaxHP" : 20,
-"Stamina" : 5,
-"magic" : 0,
-"stamina_regeneration"  : 5, 
-"race"  :  "Human"
+    "HP" : 22,
+    "MaxHP" : 20,
+    "Stamina" : 5,
+    "magic" : 0,
+    "stamina_regeneration"  : 5, 
+    "race"  :  "Human"
 }
+
+
+### ALL RACES LISTED BELOW ###
+
+RACES = ["HUMAN"]
